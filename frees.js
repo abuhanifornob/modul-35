a8a4601cd344a77fa2eef2178236136a500bff71
Abu Hanif Arnob <hanifcse90@gmail.com>
2022-08-31 13:01:07 +0600

    Object.keyw, Object.values, Object.entries & Object Remove & Delete , Object seal, Freze appy.

---
+++ frees.js
@@ -0,0 +1,32 @@
+const person = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 50,
+    eyeColor: "blue"
+};
+// all Key Return..............
+const allKeys = Object.keys(person);
+
+// all Value Return............
+const allVaues = Object.values(person);
+console.log(allKeys);
+console.log(allVaues);
+
+// Key and  Vale Arry
+const keyValue = Object.entries(person);
+console.log(keyValue);
+
+// property Delete .......
+delete person.age;
+console.log(person);
+
+//Object.seal(person); // We ditnot add or Rmove any propery only we can Modify.
+
+person.firstName = "Abu Hanif"; // afete seal modify first name property.
+console.log(person);
+
+//  object Frees.........
+
+Object.freeze(person);
+person.lastName = 'Arnob'; //  afete Freeze try to chane the last name Property , 
+console.log(person);
